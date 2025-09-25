15fb20d47004940c5ee62f8eec0df1890433c85a
ARI RIZKI RAMANDA <49813058+acthunter@users.noreply.github.com>
2025-09-25 13:51:04 +0700

    add tabel gold dml

---
+++ sql/dml/transactions_summary_202509251348.sql
@@ -0,0 +1,3 @@
+INSERT INTO gold.transactions_summary (trx_date,tipe_anomali,level_anomali,transaksi_success,transaksi_failed,total_amount_transaksi,total_transaksi,currency,created_by,created_date) VALUES
+	 ('2025-09-05','Normal','Low',2634,0,2121307000.00,2634,'IDR','Python Script','2025-09-24 09:51:42.68123'),
+	 ('2025-09-05','Abnormal','High',10,0,15413000.00,10,'IDR','Python Script','2025-09-24 09:51:42.68123');
