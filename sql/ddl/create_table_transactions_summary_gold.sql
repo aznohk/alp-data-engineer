d057dd29d93ecd5f0619085ed5df5a29156f7fb8
ARI RIZKI RAMANDA <49813058+acthunter@users.noreply.github.com>
2025-09-25 13:45:16 +0700

    Update create_table_transactions_summary_gold.sql

--- sql/ddl/create_table_transactions_summary_gold.sql
+++ sql/ddl/create_table_transactions_summary_gold.sql
@@ -1,12 +1,13 @@
 CREATE TABLE gold.transactions_summary (
-	trx_date date NULL,
-	tipe_anomali varchar(50) NULL,
-	level_anomali varchar(20) NULL,
-	transaksi_success int4 NULL,
-	transaksi_failed int4 NULL,
-	total_amount_transaksi numeric(15, 2) NULL,
-	total_transaksi int4 NULL,
-	currency varchar(10) NULL,
-	created_by varchar(255) NULL,
-	created_date timestamp DEFAULT now() NULL
+	trx_date date NOT NULL,
+	tipe_anomali varchar(20) NOT NULL,
+	level_anomali varchar(20) DEFAULT 'Low'::character varying NULL,
+	transaksi_success int4 NOT NULL,
+	transaksi_failed int4 NOT NULL,
+	total_amount_transaksi numeric(18, 2) NOT NULL,
+	total_transaksi int4 NOT NULL,
+	currency varchar(5) DEFAULT 'IDR'::character varying NULL,
+	created_by varchar(50) DEFAULT 'Python Script'::character varying NULL,
+	created_date timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	CONSTRAINT transactions_summary_pkey PRIMARY KEY (trx_date, tipe_anomali)
 );
