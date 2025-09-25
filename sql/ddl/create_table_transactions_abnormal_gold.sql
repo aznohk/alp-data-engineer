03fe524e9d2c7df8afe5d4de432bff2b14daac93
ARI RIZKI RAMANDA <49813058+acthunter@users.noreply.github.com>
2025-09-25 13:43:26 +0700

    Update create_table_transactions_abnormal_gold.sql

--- sql/ddl/create_table_transactions_abnormal_gold.sql
+++ sql/ddl/create_table_transactions_abnormal_gold.sql
@@ -1,16 +1,17 @@
 
 CREATE TABLE gold.transactions_abnormal (
-	id varchar(255) NULL,
-	account_num varchar(255) NULL,
-	trx_date date NULL,
-	total_trx int4 NULL,
-	total_amount numeric(15, 2) NULL,
-	total_debit numeric(15, 2) NULL,
-	total_credit numeric(15, 2) NULL,
-	failed_trx int4 NULL,
-	anomaly_trx int4 NULL,
-	created_by varchar(255) NULL,
-	created_date timestamp DEFAULT now() NULL,
-	updated_by varchar(255) NULL,
-	updated_date timestamp DEFAULT now() NULL
+	id uuid NOT NULL,
+	account_num varchar(50) NOT NULL,
+	trx_date date NOT NULL,
+	total_trx int4 NOT NULL,
+	total_amount numeric(18, 2) NOT NULL,
+	total_debit numeric(18, 2) NOT NULL,
+	total_credit numeric(18, 2) NOT NULL,
+	failed_trx int4 NOT NULL,
+	anomaly_trx int4 NOT NULL,
+	created_by varchar(50) NULL,
+	created_date timestamp NULL,
+	updated_by varchar(50) NULL,
+	updated_date timestamp NULL,
+	CONSTRAINT transactions_abnormal_pkey PRIMARY KEY (id)
 );
